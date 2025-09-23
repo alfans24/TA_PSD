ffbff641bffe6b8f7de843cd604eac07edf8e531
alfans24 <paupasam24@gmail.com>
2025-09-23 13:27:59 +0700

    update file

--- TP1_Penambahan Matriks 2x2.cpp
+++ TP1_Penambahan Matriks 2x2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
+    
     int a[2][2], b[2][2], c[2][2];
     cout << "Masukkan elemen matriks A (2x2):" << endl;
     for (int i = 0; i < 2; i++){
